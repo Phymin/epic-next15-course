1da229a5560ed1217584e6344911bd47e57cc011
phymon <wmm2006@163.com>
2025-01-10 11:42:32 +0800

    2nd commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import { HeroSection } from "@/components/custom/hero-section";
+
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"],
+            },
+            link: {
+              populate: true,
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  try {
+    const response = await fetch(url.href);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  console.dir(strapiData, { depth: null });
+
+  const { blocks } = strapiData.data;
+  return (
+    <main>
+      <HeroSection data={blocks[0]} />
+    </main>
+  );
+}
